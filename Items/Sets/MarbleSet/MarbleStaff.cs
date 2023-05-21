a087b4f82169bd0ef856257df599ffb40a05f77e
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2023-05-21 16:30:35 -0300

    Gilded Tome

--- Items/Sets/MarbleSet/MarbleStaff.cs
+++ Items/Sets/MarbleSet/MarbleStaff.cs
@@ -1,7 +1,11 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using SpiritMod.Projectiles.Magic;
+using System;
 using Terraria;
+using Terraria.Audio;
 using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -12,7 +16,7 @@ namespace SpiritMod.Items.Sets.MarbleSet
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Gilded Tome");
-			Tooltip.SetDefault("Rains down gilded stalactites from the sky\nThese stalactites stick to enemies and slow them down");
+			Tooltip.SetDefault("Rains down gilded stalactites from the sky");
 		}
 
 		public override void SetDefaults()
@@ -20,39 +24,124 @@ namespace SpiritMod.Items.Sets.MarbleSet
 			Item.damage = 30;
 			Item.DamageType = DamageClass.Magic;
 			Item.mana = 8;
-			Item.width = 50;
-			Item.height = 50;
-			Item.useTime = 28;
-			Item.useAnimation = 28;
+			Item.width = Item.height = 50;
+			Item.useTime = 17;
+			Item.useAnimation = 34;
 			Item.useStyle = ItemUseStyleID.Shoot;
 			Item.noMelee = true;
+			Item.noUseGraphic = true;
 			Item.knockBack = 1;
 			Item.useTurn = false;
 			Item.value = Item.sellPrice(0, 0, 50, 0);
 			Item.rare = ItemRarityID.Green;
-			Item.UseSound = SoundID.Item20;
 			Item.autoReuse = true;
+			Item.channel = true;
 			Item.shoot = ModContent.ProjectileType<MarbleStalactite>();
-			Item.shootSpeed = 20f;
-		}
-
-		public override void AddRecipes()
-		{
-			Recipe recipe = CreateRecipe(1);
-			recipe.AddIngredient(ModContent.ItemType<MarbleChunk>(), 13);
-			recipe.AddIngredient(ItemID.Book, 1);
-			recipe.AddTile(TileID.Anvils);
-			recipe.Register();
+			Item.shootSpeed = 10f;
 		}
 
 		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
 		{
 			if (Main.myPlayer == player.whoAmI)
 			{
-				Vector2 mouse = Main.MouseWorld;
-				Projectile.NewProjectile(source, mouse.X, player.Center.Y - 700 + Main.rand.Next(-50, 50), 0, Main.rand.Next(18, 28), ModContent.ProjectileType<MarbleStalactite>(), damage, knockback, player.whoAmI);
+				position = new Vector2(Main.MouseWorld.X + (15 * Main.rand.NextFloat(-1.0f, 1.0f)), player.Center.Y - (150 + (100 * Main.rand.NextFloat())));
+				velocity = (Vector2.UnitY * Main.rand.Next(12, 18)).RotatedByRandom(.15f);
+
+				Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
+				Projectile.NewProjectile(source, position, Vector2.Zero, ModContent.ProjectileType<MarblePortal>(), damage, knockback, player.whoAmI);
 			}
+
+			if (player.ItemAnimationJustStarted)
+			{
+				int heldType = ModContent.ProjectileType<MarbleStaffProj>();
+
+				if (player.ownedProjectileCounts[heldType] < 1)
+					Projectile.NewProjectile(source, player.MountedCenter, Vector2.Zero, heldType, damage, knockback, player.whoAmI);
+			}
+
 			return false;
 		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ModContent.ItemType<MarbleChunk>(), 13);
+			recipe.AddIngredient(ItemID.Book, 1);
+			recipe.AddTile(TileID.Anvils);
+			recipe.Register();
+		}
+	}
+
+	public class MarbleStaffProj : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Gilded Tome");
+			Main.projFrames[Type] = 13;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.Size = new Vector2(20);
+			Projectile.penetrate = -1;
+			Projectile.friendly = true;
+			Projectile.hostile = false;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = false;
+		}
+
+		public Player Owner => Main.player[Projectile.owner];
+
+		public override void OnSpawn(IEntitySource source)
+		{
+			if (!Main.dedServ)
+				SoundEngine.PlaySound(SoundID.Item20, Projectile.Center);
+		}
+
+		public override void AI()
+		{
+			Projectile.velocity = Vector2.Lerp(Projectile.velocity, new Vector2(Owner.direction * 18, -6 * Owner.gravDir), 0.1f);
+
+			Owner.itemRotation = MathHelper.WrapAngle(Projectile.velocity.ToRotation() + (Projectile.direction < 0 ? MathHelper.Pi : 0));
+			Owner.heldProj = Projectile.whoAmI;
+
+			Projectile.direction = Projectile.spriteDirection = Owner.direction;
+			Projectile.rotation = 0f;
+			Projectile.Center = Owner.MountedCenter + Projectile.velocity;
+
+			if (++Projectile.frameCounter >= 4)
+			{
+				Projectile.frameCounter = 0;
+
+				if (++Projectile.frame >= Main.projFrames[Type])
+					Projectile.frame = 8;
+			}
+			if (Main.rand.NextBool(10))
+			{
+				Dust.NewDustPerfect(Projectile.Center + (Main.rand.NextVector2Unit() * Main.rand.NextFloat() * 18) + (Vector2.UnitX * 8 * Projectile.direction), DustID.FireworkFountain_Yellow, Vector2.UnitY * -1.5f, 0, default, .5f).noGravity = true;
+			}
+
+			if (Owner.channel || Owner.itemAnimation > (Owner.itemAnimationMax / 2))
+				Projectile.timeLeft = 2;
+		}
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			Texture2D texture = TextureAssets.Projectile[Type].Value;
+			Rectangle drawFrame = texture.Frame(1, Main.projFrames[Type], 0, Projectile.frame);
+			SpriteEffects effects = (Projectile.spriteDirection == -1) ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+
+			for (int i = 3; i > 0; i--)
+			{
+				Color drawColor = (i == 1) ? lightColor : (Color.Goldenrod * .5f) with { A = 0 };
+				float scale = Projectile.scale + ((float)Math.Sin(Main.timeForVisualEffects / 30) * .2f * (i - 1));
+
+				Main.EntitySpriteDraw(texture, Projectile.Center - Main.screenPosition, drawFrame, Projectile.GetAlpha(drawColor), Projectile.rotation, drawFrame.Size() / 2, scale, effects, 0);
+			}
+
+			return false;
+		}
+
+		public override bool? CanDamage() => false;
 	}
 }
--- Projectiles/Magic/MarbleStalactite.cs
+++ Projectiles/Magic/MarbleStalactite.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using SpiritMod.Buffs;
+using SpiritMod.Mechanics.Trails;
+using SpiritMod.Particles;
+using System;
 using Terraria;
 using Terraria.Audio;
 using Terraria.GameContent;
@@ -9,105 +11,114 @@ using Terraria.ModLoader;
 
 namespace SpiritMod.Projectiles.Magic
 {
-	public class MarbleStalactite : ModProjectile
+	public class MarbleStalactite : ModProjectile, ITrailProjectile
 	{
+		private readonly int lingerTime = 20;
+		private int Counter
+		{
+			get => (int)Projectile.ai[0];
+			set => Projectile.ai[0] = value;
+		}
+
+		private bool Collided => Counter > 0;
+
+		public void DoTrailCreation(TrailManager trailManager)
+		{
+			trailManager.CreateTrail(Projectile, new GradientTrail(Color.DarkGoldenrod with { A = 0 }, Color.Transparent), new RoundCap(), new DefaultTrailPosition(), 25, 200, new DefaultShader());
+			trailManager.CreateTrail(Projectile, new StandardColorTrail(Color.PaleGoldenrod with { A = 0 }), new RoundCap(), new DefaultTrailPosition(), 50, 160, new ImageShader(Mod.Assets.Request<Texture2D>("Textures/Trails/Trail_3", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value, 0.05f, 1f, 1f));
+		}
+
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Gilded Stalactite");
-			ProjectileID.Sets.TrailCacheLength[Projectile.type] = 4;
+			ProjectileID.Sets.TrailCacheLength[Projectile.type] = 5;
 			ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
 		}
 
 		public override void SetDefaults()
 		{
-			Projectile.width = 14;
-			Projectile.height = 18;
+			Projectile.Size = new Vector2(30, 30);
 			Projectile.DamageType = DamageClass.Magic;
 			Projectile.friendly = true;
 			Projectile.penetrate = -1;
+			Projectile.tileCollide = false;
+			Projectile.extraUpdates = 1;
 		}
 
-		public override bool PreAI()
+		public override void AI()
 		{
-			int num = 5;
-			for (int k = 0; k < 3; k++)
+			if (!Collided)
 			{
-				int index2 = Dust.NewDust(Projectile.position, 1, 1, DustID.FireworkFountain_Yellow, 0.0f, 0.0f, 0, new Color(), 1f);
-				Main.dust[index2].position = Projectile.Center - Projectile.velocity / num * (float)k;
-				Main.dust[index2].scale = .5f;
-				Main.dust[index2].velocity *= 0f;
-				Main.dust[index2].noGravity = true;
-				Main.dust[index2].noLight = false;
+				for (int k = 0; k < 2; k++)
+				{
+					Dust dust = Dust.NewDustPerfect(Projectile.Center, DustID.FireworkFountain_Yellow, (Projectile.velocity * Main.rand.NextFloat() * .2f).RotatedByRandom(1f), 0, default, .2f);
+					dust.noGravity = true;
+					dust.noLight = false;
+				}
+
+				if (WorldGen.SolidOrSlopedTile(Framing.GetTileSafely(Projectile.Center)))
+				{
+					ProjectileExtras.Explode(Projectile.whoAmI, 80, 80, delegate 
+					{
+						for (int j = 0; j < 12; j++)
+						{
+							if (j < 6)
+							{
+								Dust dust = Dust.NewDustPerfect(Projectile.position + new Vector2(Projectile.width * Main.rand.NextFloat(0.0f, 1.0f), Projectile.height / 2), DustID.FireworkFountain_Yellow, new Vector2(8f * Main.rand.NextFloat(-1.0f, 1.0f), 0), 0, default, 0.8f);
+								dust.noGravity = true;
+								dust.fadeIn = 1.2f;
+							}
+							else
+							{
+								Dust.NewDustPerfect(Projectile.position + new Vector2(Projectile.width * Main.rand.NextFloat(0.0f, 1.0f), Projectile.height / 2), DustID.FireworkFountain_Yellow, (-Projectile.velocity * Main.rand.NextFloat()).RotatedByRandom(MathHelper.Pi), 0, default, Main.rand.NextFloat(0.5f, 1.0f));
+
+								if (!Main.dedServ)
+								{
+									Vector2 randomPos = Projectile.Center + Main.rand.NextVector2Unit() * 15;
+									ParticleHandler.SpawnParticle(new ImpactLine(randomPos, Projectile.Center.DirectionTo(randomPos) * Main.rand.NextFloat(1.0f, 3.0f), Color.Lerp(Color.White, Color.Goldenrod, Main.rand.NextFloat(0.0f, 1.0f)), new Vector2(0.5f, Main.rand.NextFloat(0.5f, 1.0f)), 18)
+									{ Rotation = randomPos.AngleTo(Projectile.Center) });
+								}
+							}
+						}
+
+						for (int i = 0; i < 18; i++)
+						{
+							int num = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.Smoke, 0f, -1f, 100, default, 1.5f);
+							Main.dust[num].velocity *= 1.2f;
+						}
+
+						for (int k = 0; k < 4; k++)
+						{
+							float scaleFactor = 0.4f;
+							if (k > 3)
+								scaleFactor = 0.8f;
+
+							Gore gore = Main.gore[Gore.NewGore(Projectile.GetSource_Death("Explosion"), Projectile.position, default, Main.rand.Next(61, 64), 1f)];
+							gore.velocity *= scaleFactor;
+
+							float negateX = k > 2 ? -1 : 1;
+
+							gore.velocity.X += 1f * negateX;
+							gore.velocity.Y -= Main.rand.NextFloat() * 2f;
+						}
+					});
+
+					Counter++;
+					Projectile.netUpdate = true;
+				}
 			}
-			if (Projectile.ai[0] == 0)
-				Projectile.rotation = 0f;
 			else
 			{
-				Projectile.ignoreWater = true;
-				Projectile.tileCollide = false;
-				int num996 = 15;
-				bool flag52 = false;
-				bool flag53 = false;
-				Projectile.localAI[0] += 1f;
-				if (Projectile.localAI[0] % 30f == 0f)
-					flag53 = true;
-
-				int num997 = (int)Projectile.ai[1];
-				if (Projectile.localAI[0] >= (float)(60 * num996))
-					flag52 = true;
-				else if (num997 < 0 || num997 >= 200)
-					flag52 = true;
-				else if (Main.npc[num997].active && !Main.npc[num997].dontTakeDamage)
-				{
-					Projectile.Center = Main.npc[num997].Center - Projectile.velocity * 2f;
-					Projectile.gfxOffY = Main.npc[num997].gfxOffY;
-
-					if (flag53)
-						Main.npc[num997].HitEffect(0, 1.0);
-				}
-				else
-					flag52 = true;
-
-				if (flag52)
+				if (++Counter >= lingerTime)
 					Projectile.Kill();
 			}
-			return false;
+
+			Projectile.alpha = (int)MathHelper.Max(0, Projectile.alpha - (255 / 20));
+			Projectile.rotation = Projectile.velocity.ToRotation() - 1.57f;
 		}
 
-		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
-		{
-			target.AddBuff(ModContent.BuffType<GildedSlow>(), 210);
-			Projectile.ai[0] = 1f;
-			Projectile.ai[1] = (float)target.whoAmI;
-			Projectile.velocity = (target.Center - Projectile.Center) * 0.75f;
-			Projectile.netUpdate = true;
-			Projectile.damage = 0;
+		public override bool ShouldUpdatePosition() => !Collided;
 
-			int num31 = 1;
-			Point[] array2 = new Point[num31];
-			int num32 = 0;
-
-			for (int n = 0; n < 1000; n++)
-			{
-				if (n != Projectile.whoAmI && Main.projectile[n].active && Main.projectile[n].owner == Main.myPlayer && Main.projectile[n].type == Projectile.type && Main.projectile[n].ai[0] == 1f && Main.projectile[n].ai[1] == target.whoAmI)
-				{
-					array2[num32++] = new Point(n, Main.projectile[n].timeLeft);
-					if (num32 >= array2.Length)
-						break;
-				}
-			}
-
-			if (num32 >= array2.Length)
-			{
-				int num33 = 0;
-				for (int num34 = 1; num34 < array2.Length; num34++)
-				{
-					if (array2[num34].Y < array2[num33].Y)
-						num33 = num34;
-				}
-				Main.projectile[array2[num33].X].Kill();
-			}
-		}
 		public override void Kill(int timeLeft)
 		{
 			SoundEngine.PlaySound(SoundID.Dig, Projectile.Center);
@@ -117,15 +128,97 @@ namespace SpiritMod.Projectiles.Magic
 				Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Marble, 2.5f * 1, -2.5f, 0, default, 0.37f);
 			}
 		}
+
 		public override bool PreDraw(ref Color lightColor)
 		{
-			Vector2 drawOrigin = new Vector2(TextureAssets.Projectile[Projectile.type].Value.Width * 0.5f, Projectile.height * 0.5f);
+			Vector2 drawOrigin = TextureAssets.Projectile[Projectile.type].Size() / 2;
 			for (int k = 0; k < Projectile.oldPos.Length; k++)
 			{
-				Vector2 drawPos = Projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+				Vector2 drawPos = Projectile.oldPos[k] - Main.screenPosition + (Projectile.Size / 2) + new Vector2(0f, Projectile.gfxOffY);
 				Color color = Projectile.GetAlpha(lightColor) * ((float)(Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
-				Main.spriteBatch.Draw(TextureAssets.Projectile[Projectile.type].Value, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0f);
+				Main.EntitySpriteDraw(TextureAssets.Projectile[Projectile.type].Value, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
 			}
+
+			Main.EntitySpriteDraw(TextureAssets.Projectile[Projectile.type].Value, Projectile.Center - Main.screenPosition, null, Projectile.GetAlpha(lightColor), Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
+
+			return false;
+		}
+	}
+
+	public class MarblePortal : ModProjectile
+	{
+		public override string Texture => "SpiritMod/Textures/StardustPillarStar";
+		public override void SetStaticDefaults() => DisplayName.SetDefault("Marble Portal");
+
+		public const float MaxScale = 0.8f;
+
+		public override void SetDefaults()
+		{
+			Projectile.Size = Vector2.One;
+			Projectile.tileCollide = false;
+			Projectile.friendly = true;
+			Projectile.ignoreWater = true;
+			Projectile.DamageType = DamageClass.Magic;
+			Projectile.scale = MaxScale * .5f;
+			Projectile.timeLeft = 40;
+		}
+
+		public override bool? CanDamage() => false;
+
+		public override void AI()
+		{
+			Projectile.rotation = 1.57f;
+
+			float fadeTime = 20f;
+
+			if (Projectile.timeLeft < fadeTime)
+			{
+				if ((Projectile.scale -= (float)(1f / fadeTime)) <= 0)
+					Projectile.Kill();
+			}
+			else
+			{
+				Projectile.scale = Math.Min(Projectile.scale + (float)(1f / fadeTime), MaxScale);
+			}
+
+			if (Main.rand.NextBool(3) && Projectile.scale > .25f && !Main.dedServ)
+				ParticleHandler.SpawnParticle(new FireParticle(Projectile.Center + (Vector2.UnitX * Main.rand.NextFloat(-30.0f, 20.0f) * Projectile.scale), Vector2.UnitY * Main.rand.NextFloat() * 3f, Color.White, Color.Gold, Main.rand.NextFloat(0.1f, 0.35f), Main.rand.Next(15, 25)));
+		}
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			Main.spriteBatch.End();
+			Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, default, default, default, default, Main.GameViewMatrix.ZoomMatrix);
+			Texture2D tex = TextureAssets.Projectile[Projectile.type].Value;
+			SpiritMod.ShaderDict["PortalShader"].Parameters["PortalNoise"].SetValue(Mod.Assets.Request<Texture2D>("Utilities/Noise/SpiralNoise").Value);
+			SpiritMod.ShaderDict["PortalShader"].Parameters["DistortionNoise"].SetValue(Mod.Assets.Request<Texture2D>("Utilities/Noise/noise").Value);
+			SpiritMod.ShaderDict["PortalShader"].Parameters["Timer"].SetValue((float)Main.timeForVisualEffects / 60f % MathHelper.TwoPi);
+			SpiritMod.ShaderDict["PortalShader"].Parameters["DistortionStrength"].SetValue(0.1f);
+			SpiritMod.ShaderDict["PortalShader"].Parameters["Rotation"].SetValue((float)Main.timeForVisualEffects / 40f % MathHelper.TwoPi);
+			SpiritMod.ShaderDict["PortalShader"].CurrentTechnique.Passes[0].Apply();
+
+			float opacitymod = (float)Math.Pow(Projectile.scale / MaxScale, 2);
+
+			float numtodraw = 3;
+			for (float i = 0; i < numtodraw; i++) //pulsating bloom type effect, draws multiple of the texture that grow in scale over time and fade out
+			{
+				float Timer = (Main.GlobalTimeWrappedHourly / 2 + i / numtodraw) % 1;
+				float Opacity = 0.33f * (float)Math.Pow(Math.Sin(Timer * MathHelper.Pi), 3);
+				float Scale = 0.6f * (Projectile.scale + (Timer * Projectile.scale));
+				float Rotation = Main.GlobalTimeWrappedHourly / 2 * ((i % 2 == 0) ? -1 : 1) * (MathHelper.TwoPi * i / numtodraw);
+
+				Main.spriteBatch.Draw(tex, Projectile.Center - Main.screenPosition, null, Color.Gold * Opacity * opacitymod,
+					Rotation, tex.Size() / 2, Scale, SpriteEffects.None, 0);
+			}
+
+			Vector2 ellipticalscale = new Vector2(Projectile.scale * 0.5f, Projectile.scale);
+			Main.spriteBatch.Draw(tex, Projectile.Center - Main.screenPosition, null, Color.PaleGoldenrod * opacitymod, Projectile.rotation, tex.Size() / 2, //the main "body" of the portal
+				ellipticalscale, SpriteEffects.None, 0);
+			Main.spriteBatch.Draw(tex, Projectile.Center + (Vector2.UnitX * 3).RotatedBy(Projectile.rotation) - Main.screenPosition, null, Color.DarkGoldenrod * opacitymod,
+				Projectile.rotation, tex.Size() / 2, ellipticalscale * 0.66f, SpriteEffects.None, 0);  //the center, moves slightly depending on direction for illusion of facing the player
+
+			Main.spriteBatch.End();
+			Main.spriteBatch.Begin(default, BlendState.AlphaBlend, default, default, default, default, Main.GameViewMatrix.ZoomMatrix);
 			return false;
 		}
 	}
