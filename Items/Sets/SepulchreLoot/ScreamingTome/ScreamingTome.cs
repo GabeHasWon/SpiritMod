d955d0edb94b6ca9bd34e374cb9743ffd9905bb7
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2023-05-28 12:38:33 -0300

    Various Vin resprites + readded Overseer

--- Items/Sets/SepulchreLoot/ScreamingTome/ScreamingTome.cs
+++ Items/Sets/SepulchreLoot/ScreamingTome/ScreamingTome.cs
@@ -1,6 +1,11 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SpiritMod.Items.Sets.MarbleSet;
+using System;
 using Terraria;
+using Terraria.Audio;
 using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -20,7 +25,8 @@ namespace SpiritMod.Items.Sets.SepulchreLoot.ScreamingTome
         {
             Item.damage = 16;
             Item.noMelee = true;
-            Item.DamageType = DamageClass.Magic;
+			Item.noUseGraphic = true;
+			Item.DamageType = DamageClass.Magic;
             Item.width = 36;
             Item.height = 40;
             Item.useTime = 35;
@@ -42,9 +48,83 @@ namespace SpiritMod.Items.Sets.SepulchreLoot.ScreamingTome
 
 		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) 
         {
-            int p = Projectile.NewProjectile(source, player.Center.X - Main.rand.Next(-50, 50), player.Center.Y - Main.rand.Next(-50, 50), 0f, 0f, type, damage, knockback, player.whoAmI);
-            Main.projectile[p].ai[0] = player.whoAmI;
-            return false;
+            Projectile.NewProjectile(source, player.Center - (Main.rand.NextVector2Unit() * Main.rand.NextFloat(0.0f, 50.0f)), Vector2.Zero, type, damage, knockback, player.whoAmI, player.whoAmI);
+
+			int heldType = ModContent.ProjectileType<ScreamingTomeProj>();
+
+			if (player.ownedProjectileCounts[heldType] < 1)
+				Projectile.NewProjectile(source, player.MountedCenter, Vector2.Zero, heldType, damage, knockback, player.whoAmI);
+
+			return false;
         }
-    }
+
+		public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
+			=> GlowmaskUtils.DrawItemGlowMaskWorld(Main.spriteBatch, Item, ModContent.Request<Texture2D>(Texture + "_Glow").Value, rotation, scale);
+	}
+
+	public class ScreamingTomeProj : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Screaming Tome");
+			Main.projFrames[Type] = 5;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.Size = new Vector2(20);
+			Projectile.penetrate = -1;
+			Projectile.friendly = true;
+			Projectile.hostile = false;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = false;
+		}
+
+		public Player Owner => Main.player[Projectile.owner];
+
+		public override void OnSpawn(IEntitySource source)
+		{
+			if (!Main.dedServ)
+				SoundEngine.PlaySound(new SoundStyle("SpiritMod/Sounds/PageFlip") with { PitchVariance = 0.3f, Volume = 0.65f }, Owner.Center);
+		}
+
+		public override void AI()
+		{
+			Projectile.velocity = Vector2.Lerp(Projectile.velocity, new Vector2(Owner.direction * 10, -2 * Owner.gravDir), 0.1f);
+
+			Owner.itemRotation = MathHelper.WrapAngle(Projectile.velocity.ToRotation() + (Projectile.direction < 0 ? MathHelper.Pi : 0));
+			Owner.heldProj = Projectile.whoAmI;
+
+			Projectile.direction = Projectile.spriteDirection = Owner.direction;
+			Projectile.rotation = 0f;
+			Projectile.Center = Owner.MountedCenter + Projectile.velocity;
+
+			float rotation = Projectile.velocity.ToRotation() - (1.57f - (.785f * Projectile.direction));
+			Owner.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.ThreeQuarters, rotation);
+			Owner.SetCompositeArmBack(true, Player.CompositeArmStretchAmount.ThreeQuarters, rotation);
+
+			if (++Projectile.frameCounter >= 4)
+			{
+				Projectile.frameCounter = 0;
+				Projectile.frame = ++Projectile.frame % Main.projFrames[Type];
+			}
+
+			if (Owner.channel)
+				Projectile.timeLeft = 2;
+		}
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			Texture2D texture = TextureAssets.Projectile[Type].Value;
+			Rectangle drawFrame = texture.Frame(1, Main.projFrames[Type], 0, Projectile.frame, 0, -2);
+			SpriteEffects effects = (Projectile.spriteDirection == -1) ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+
+			Main.EntitySpriteDraw(texture, Projectile.Center - Main.screenPosition, drawFrame, Projectile.GetAlpha(lightColor), Projectile.rotation, drawFrame.Size() / 2, Projectile.scale, effects, 0);
+			Main.EntitySpriteDraw(ModContent.Request<Texture2D>(Texture + "_Glow").Value, Projectile.Center - Main.screenPosition, drawFrame, Projectile.GetAlpha(Color.White), Projectile.rotation, drawFrame.Size() / 2, Projectile.scale, effects, 0);
+
+			return false;
+		}
+
+		public override bool? CanDamage() => false;
+	}
 }
---
+++ Projectiles/Pet/OverseerPet.cs
@@ -0,0 +1,52 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SpiritMod.GlobalClasses.Players;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod.Projectiles.Pet
+{
+	public class OverseerPet : ModProjectile
+	{
+		private float frameCounter;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Overseer");
+			Main.projFrames[Projectile.type] = 3;
+			Main.projPet[Projectile.type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.CloneDefaults(ProjectileID.ZephyrFish);
+			AIType = ProjectileID.ZephyrFish;
+			Projectile.width = 40;
+			Projectile.height = 30;
+		}
+
+		public override void AI()
+		{
+			Player player = Main.player[Projectile.owner];
+
+			player.GetModPlayer<PetPlayer>().PetFlag(Projectile);
+			player.zephyrfish = false; //Relic from AIType
+
+			Projectile.frame = (int)(frameCounter += .2f) % Main.projFrames[Type];
+		}
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			Texture2D texture = TextureAssets.Projectile[Type].Value;
+			Rectangle frame = texture.Frame(1, Main.projFrames[Type], 0, Projectile.frame, 0, -2);
+			SpriteEffects effects = (Projectile.spriteDirection == -1) ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+
+			Main.EntitySpriteDraw(texture, Projectile.Center - Main.screenPosition, frame, Projectile.GetAlpha(lightColor), Projectile.rotation, frame.Size() / 2, Projectile.scale, effects, 0);
+			Main.EntitySpriteDraw(ModContent.Request<Texture2D>(Texture + "_Glow").Value, Projectile.Center - Main.screenPosition, frame, Projectile.GetAlpha(Color.White), Projectile.rotation, frame.Size() / 2, Projectile.scale, effects, 0);
+
+			return false;
+		}
+	}
+}
