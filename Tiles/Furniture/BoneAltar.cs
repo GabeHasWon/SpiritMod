27b2f621b62ed0f6d9589bb6e861b3d152656eaa
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2023-06-02 01:22:04 -0300

    Cleanup

--- Tiles/Furniture/BoneAltar.cs
+++ Tiles/Furniture/BoneAltar.cs
@@ -81,22 +81,22 @@ namespace SpiritMod.Tiles.Furniture
 				Main.dust[index1].velocity = Vector2.Zero;
 			}
 
-			if (!NPC.AnyNPCs(ModContent.NPCType<ForestWraith>()))
-			{
-				if (Main.netMode == NetmodeID.SinglePlayer)
-				{
-					Main.NewText("You have disturbed the ancient Nature Spirits!", 0, 170, 60);
+			if (NPC.AnyNPCs(ModContent.NPCType<ForestWraith>()))
+				return;
 
-					int who = NPC.NewNPC(new EntitySource_TileBreak(i, j), i * 16, j * 16 - 300, ModContent.NPCType<ForestWraith>(), 0, 2, 1, 0, 0);
-					Main.npc[who].netUpdate2 = true;
-				}
-				else if (Main.netMode == NetmodeID.MultiplayerClient)
-				{
-					ModPacket packet = SpiritMod.Instance.GetPacket(MessageType.SpawnGladeWraith, 2);
-					packet.Write(i * 16);
-					packet.Write(j * 16 - 300);
-					packet.Send();
-				}
+			if (Main.netMode == NetmodeID.SinglePlayer)
+			{
+				Main.NewText("You have disturbed the ancient Nature Spirits!", 0, 170, 60);
+
+				int who = NPC.NewNPC(new EntitySource_TileBreak(i, j), i * 16, j * 16 - 300, ModContent.NPCType<ForestWraith>(), 0, 2, 1, 0, 0);
+				Main.npc[who].netUpdate2 = true;
+			}
+			else if (Main.netMode == NetmodeID.MultiplayerClient)
+			{
+				ModPacket packet = SpiritMod.Instance.GetPacket(MessageType.SpawnGladeWraith, 2);
+				packet.Write(i * 16);
+				packet.Write(j * 16 - 300);
+				packet.Send();
 			}
 		}
 	}
