8b9c90d91c42b41a631305173e359afa68b93910
GabeHasWon <gabriel@guevarameyer.com>
2023-06-29 12:57:04 -0500

    bumped up packing crate spawn rate

--- Items/Placeable/FishCrate.cs
+++ Items/Placeable/FishCrate.cs
@@ -15,30 +15,9 @@ namespace SpiritMod.Items.Placeable
 	[Sacrifice(10)]
 	public class FishCrate : FloatingItem
 	{
-		public override float SpawnWeight => QuestManager.GetQuest<AnglerStatueQuest>().IsActive && AnyOfSelf() ? 4f : 0.08f;
+		public override float SpawnWeight => QuestManager.GetQuest<AnglerStatueQuest>().IsActive ? 4f : 0.4f;
 		public override float Weight => base.Weight * 0.9f;
 		public override float Bouyancy => base.Bouyancy * 1.05f;
-
-		private bool AnyOfSelf()
-		{
-			bool leftSide = true;
-			bool rightSide = true;
-
-			for (int i = 0; i < Main.maxItems; ++i)
-			{
-				if (Main.item[i].active && Main.item[i].type == Type)
-				{
-					if (Main.item[i].position.X < Main.maxTilesX * 8)
-						leftSide = true;
-					else
-						rightSide = true;
-
-					if (leftSide && rightSide)
-						return true;
-				}
-			}
-			return false;
-		}
 
 		public override void SetStaticDefaults()
 		{
