49c94f248c642457b805a50bd38742d100442e2f
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2023-07-19 23:28:13 -0300

    Marble and granite sculptures

---
+++ Items/Placeable/Furniture/Sculptures/GraniteSculpture3x5Item.cs
@@ -0,0 +1,36 @@
+using SpiritMod.Tiles.Furniture.Sculptures;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod.Items.Placeable.Furniture.Sculptures
+{
+	[Sacrifice(1)]
+	public class GraniteSculpture3x5Item : ModItem
+	{
+		public override void SetStaticDefaults() => DisplayName.SetDefault("Angular Granite Sculpture");
+
+		public override void SetDefaults()
+		{
+			Item.width = 36;
+			Item.height = 34;
+			Item.value = 150;
+			Item.maxStack = 99;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.useTime = 10;
+			Item.useAnimation = 15;
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.consumable = true;
+			Item.createTile = ModContent.TileType<GraniteSculpture3x5>();
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.GraniteBlock, 25);
+			recipe.AddTile(TileID.HeavyWorkBench);
+			recipe.Register();
+		}
+	}
+}
---
+++ Tiles/Furniture/Sculptures/GraniteSculpture1x2.cs
@@ -0,0 +1,32 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.ID;
+using Terraria.DataStructures;
+using SpiritMod.Items.Placeable.Furniture.Sculptures;
+
+namespace SpiritMod.Tiles.Furniture.Sculptures
+{
+	public class GraniteSculpture1x2 : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoAttach[Type] = true;
+			Main.tileLavaDeath[Type] = false;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style1x2);
+			TileObjectData.newTile.Width = 1;
+			TileObjectData.newTile.Height = 2;
+			TileObjectData.newTile.CoordinateHeights = new int[] { 16, 18 };
+			TileObjectData.addTile(Type);
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Granite Sculpture");
+			AddMapEntry(new Color(140, 140, 140), name);
+			DustType = DustID.Granite;
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY) 
+			=> Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 48, 48, ModContent.ItemType<GraniteSculpture1x2Item>());
+	}
+}
