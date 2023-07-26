0f33f864b8660295d94128ed73562dc77891de72
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2023-07-26 17:03:27 -0300

    Starcharger

--- Items/BossLoot/StarplateDrops/SteamplateBow/SteamplateBow.cs
+++ Items/BossLoot/StarplateDrops/SteamplateBow/SteamplateBow.cs
@@ -1,7 +1,12 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using SpiritMod.Particles;
 using SpiritMod.Projectiles.Arrow;
+using SpiritMod.Projectiles.BaseProj;
 using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -23,8 +28,7 @@ namespace SpiritMod.Items.BossLoot.StarplateDrops.SteamplateBow
 			Item.DamageType = DamageClass.Ranged;
 			Item.width = 28;
 			Item.height = 36;
-			Item.useTime = 26;
-			Item.useAnimation = 26;
+			Item.useTime = Item.useAnimation = 26;
 			Item.useStyle = ItemUseStyleID.Shoot;
 			Item.shoot = ProjectileID.WoodenArrowFriendly;
 			Item.useAmmo = AmmoID.Arrow;
@@ -33,27 +37,54 @@ namespace SpiritMod.Items.BossLoot.StarplateDrops.SteamplateBow
 			Item.UseSound = SoundID.Item5;
 			Item.value = Item.sellPrice(0, 1, 0, 0);
 			Item.autoReuse = true;
-			Item.shootSpeed = 15f;
+			Item.shootSpeed = 12f;
 		}
 
 		public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI) => GlowmaskUtils.DrawItemGlowMaskWorld(spriteBatch, Item, ModContent.Request<Texture2D>(Texture + "_Glow").Value, rotation, scale);
+		
 		public override Vector2? HoldoutOffset() => new Vector2(-4, 0);
+		
 		public override bool AltFunctionUse(Player player) => true;
+		
 		public override void HoldItem(Player player) => player.GetModPlayer<SteamplateBowPlayer>().active = true;
 
-		public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+		public override bool CanUseItem(Player player)
 		{
-			SteamplateBowPlayer modPlayer = player.GetModPlayer<SteamplateBowPlayer>();
-			if (player.altFunctionUse == 2)
+			if (player.IsUsingAlt())
 			{
-				type = ModContent.ProjectileType<NegativeArrow>();
-				modPlayer.negative = true;
+				Item.noUseGraphic = false;
+				Item.UseSound = SoundID.Item5;
+				Item.useTime = Item.useAnimation = 26;
 			}
 			else
 			{
-				type = ModContent.ProjectileType<PositiveArrow>();
-				modPlayer.negative = false;
+				Item.noUseGraphic = true;
+				Item.UseSound = null;
+				Item.useTime = Item.useAnimation = 36;
 			}
+			return true;
+		}
+
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+		{
+			SteamplateBowPlayer modPlayer = player.GetModPlayer<SteamplateBowPlayer>();
+			if (player.IsUsingAlt())
+			{
+				type = ModContent.ProjectileType<NegativeArrow>();
+				modPlayer.negative = true;
+
+				Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
+			}
+			else
+			{
+				//type = ModContent.ProjectileType<PositiveArrow>();
+				modPlayer.negative = false;
+
+				int heldType = ModContent.ProjectileType<SteamplateBowProj>();
+				if (player.ownedProjectileCounts[heldType] < 1)
+					Projectile.NewProjectile(Entity.Source_ShootWithAmmo(player), position, velocity, heldType, (int)(damage * .3f), knockback, player.whoAmI);
+			}
+			return false;
 		}
 
 		public override void AddRecipes()
@@ -62,6 +93,89 @@ namespace SpiritMod.Items.BossLoot.StarplateDrops.SteamplateBow
 				.AddIngredient(ModContent.ItemType<CosmiliteShard>(), 17)
 				.AddTile(TileID.Anvils)
 				.Register();
+		}
+	}
+
+	public class SteamplateBowProj : BaseHeldProj
+	{
+		private ref float AiTimer => ref Projectile.ai[0];
+
+		private int AiTimerMax => Owner.itemTimeMax;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Starcharger");
+			Main.projFrames[Type] = 6;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.Size = new Vector2(26);
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Ranged;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = false;
+		}
+
+		public override bool? CanDamage() => false;
+
+		public override bool? CanCutTiles() => false;
+
+		public override void AbstractAI()
+		{
+			const int delayTime = 12;
+
+			if ((++AiTimer > AiTimerMax) || Owner.dead || !Owner.active)
+			{
+				if (AiTimer > (AiTimerMax + delayTime))
+				{
+					Projectile.Kill();
+					Owner.itemTime = Owner.itemAnimation = 0;
+				}
+			}
+			else
+			{
+				const int numShots = 3;
+				Projectile.frame = (int)(AiTimer / AiTimerMax * Main.projFrames[Type] * numShots) % Main.projFrames[Type];
+
+				if (AiTimer % (AiTimerMax / numShots) == 0)
+				{
+					Vector2 pos = Projectile.Center + (Projectile.velocity * 14f);
+
+					SoundEngine.PlaySound(SoundID.Item5, Projectile.Center);
+					ParticleHandler.SpawnParticle(new StarParticle(pos, Vector2.Zero, Color.Cyan, .25f, 10, 0));
+					for (int i = 0; i < 5; i++)
+						Dust.NewDustPerfect(pos, DustID.Electric, (Projectile.velocity * Main.rand.NextFloat(.5f, 3f)).RotatedByRandom(1f), 0, default, Main.rand.NextFloat(.5f, 1f)).noGravity = true;
+
+					if (Owner.whoAmI == Main.myPlayer)
+						Projectile.NewProjectile(Entity.GetSource_FromAI(), pos, Projectile.velocity * Owner.HeldItem.shootSpeed, ModContent.ProjectileType<PositiveArrow>(), Projectile.damage, Projectile.knockBack, Owner.whoAmI);
+				} //Fire
+			}
+
+			Player.CompositeArmStretchAmount amount = Projectile.frame switch
+			{
+				1 => Player.CompositeArmStretchAmount.Full,
+				2 => Player.CompositeArmStretchAmount.ThreeQuarters,
+				3 => Player.CompositeArmStretchAmount.Quarter,
+				_ => Player.CompositeArmStretchAmount.None
+			};
+
+			Owner.SetCompositeArmFront(true, amount, Projectile.rotation - (1.57f * Projectile.direction));
+			Owner.SetCompositeArmBack(true, Player.CompositeArmStretchAmount.Full, Projectile.rotation - (1f * Projectile.direction));
+		}
+
+		public override Vector2 HoldoutOffset() => (Vector2.UnitX * 6f).RotatedBy(Projectile.velocity.ToRotation());
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			Texture2D texture = TextureAssets.Projectile[Type].Value;
+			SpriteEffects effects = (Projectile.spriteDirection == -1) ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+			Rectangle frame = texture.Frame(1, Main.projFrames[Type], 0, Projectile.frame, 0, -2);
+
+			Main.EntitySpriteDraw(texture, Projectile.Center - Main.screenPosition, frame, Projectile.GetAlpha(lightColor), Projectile.rotation, frame.Size() / 2, Projectile.scale, effects, 0);
+			Main.EntitySpriteDraw(ModContent.Request<Texture2D>(Texture + "_Glow").Value, Projectile.Center - Main.screenPosition, frame, Projectile.GetAlpha(Color.White), Projectile.rotation, frame.Size() / 2, Projectile.scale, effects, 0);
+
+			return false;
 		}
 	}
 
--- Projectiles/Arrow/NegativeArrow.cs
+++ Projectiles/Arrow/NegativeArrow.cs
@@ -1,19 +1,77 @@
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
 using SpiritMod.Mechanics.Trails;
+using Terraria;
+using System.Linq;
+using Terraria.Audio;
+using Terraria.ID;
+using SpiritMod.Particles;
+using SpiritMod.Items.BossLoot.StarplateDrops.SteamplateBow;
+using SpiritMod.Items.Accessory.UnstableTeslaCoil;
 
 namespace SpiritMod.Projectiles.Arrow
 {
-	public class NegativeArrow : PositiveArrow, ITrailProjectile
+	public class NegativeArrow : ModProjectile, ITrailProjectile
 	{
 		public override void SetStaticDefaults() => DisplayName.SetDefault("Negative Arrow");
 
-		new public void DoTrailCreation(TrailManager tM) => tM.CreateTrail(Projectile, new StandardColorTrail(new Color(255, 113, 36)), new RoundCap(), new ZigZagTrailPosition(3f), 8f, 250f);
+		public void DoTrailCreation(TrailManager tM) => tM.CreateTrail(Projectile, new StandardColorTrail(new Color(255, 113, 36, 0)), new RoundCap(), new ZigZagTrailPosition(3f), 8f, 250f);
 
 		public override void SetDefaults()
 		{
-			base.SetDefaults();
-			oppositearrow = ModContent.ProjectileType<PositiveArrow>();
+			Projectile.Size = new Vector2(16);
+			Projectile.DamageType = DamageClass.Ranged;
+			Projectile.friendly = true;
+		}
+
+		public override void AI()
+		{
+			Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+			Projectile.velocity.Y += .15f;
+
+			var target = Main.projectile.Where(x => x.active && x.type == ModContent.ProjectileType<PositiveArrow>() && x.Distance(Projectile.Center) < 240).FirstOrDefault();
+			if (target != default)
+				Projectile.velocity = Vector2.Lerp(Projectile.velocity, Projectile.DirectionTo(target.Center) * 10f, .08f);
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			for (int i = 0; i < 8; i++)
+			{
+				Vector2 dustVel = Projectile.velocity * Main.rand.NextFloat();
+				Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.FireworkFountain_Yellow, dustVel.X, dustVel.Y).noGravity = true;
+			}
+
+			var projTarget = Main.projectile.Where(x => x.active && x.type == ModContent.ProjectileType<PositiveArrow>() && x.Distance(Projectile.Center) < (Projectile.Size.Length() * 2)).OrderBy(x => Vector2.Distance(x.Center, Projectile.Center)).FirstOrDefault();
+			if (projTarget == default)
+				return;
+
+			ProjectileExtras.Explode(Projectile.whoAmI, 80, 80, delegate
+			{
+				SoundEngine.PlaySound(SoundID.Item14, Projectile.Center);
+				SoundEngine.PlaySound(new SoundStyle("SpiritMod/Sounds/starHit") with { Pitch = 1f }, Projectile.Center);
+
+				for (int i = 0; i < 30; i++)
+				{
+					Dust dust = Dust.NewDustPerfect(Projectile.Center, DustID.Electric, Main.rand.NextVector2Unit() * Main.rand.NextFloat() * 7f);
+					if (Main.rand.NextBool(2))
+						dust.noGravity = true;
+				}
+
+				DustHelper.DrawStar(new Vector2(Projectile.Center.X, Projectile.Center.Y), 226, pointAmount: 5, mainSize: 2.5f, dustDensity: 2, pointDepthMult: 0.3f, noGravity: true);
+				ParticleHandler.SpawnParticle(new ElectricalBurst(Projectile.Center, 1, 0));
+			});
+
+			int npcsHit = 0;
+			var npcTargets = Main.npc.Where(x => x.active && x.Distance(Projectile.Center) < 300 && x.CanBeChasedBy(Projectile) && Collision.CanHit(Projectile, x));
+			foreach (NPC npc in npcTargets)
+			{
+				Projectile.NewProjectile(Entity.GetSource_Death(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<Unstable_Tesla_Coil_Projectile>(), Projectile.damage, 0f, Projectile.owner, npc.position.X, npc.position.Y);
+				if (++npcsHit >= 2)
+					break;
+			}
+
+			projTarget.Kill();
 		}
 	}
 }
