6ff2eaaef3adefe50e3f003b7d66c0dc36fb470a
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2023-08-23 21:59:36 -0300

    The Good and The Bad

---
+++ Items/Placeable/Furniture/Paintings/TheGoodPaintingItem.cs
@@ -0,0 +1,27 @@
+using SpiritMod.Tiles.Furniture.Paintings;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod.Items.Placeable.Furniture.Paintings
+{
+	[Sacrifice(1)]
+	public class TheGoodPaintingItem : ModItem
+	{
+		public override void SetStaticDefaults() => ItemID.Sets.ShimmerTransformToItem[Type] = ModContent.ItemType<TheBadPaintingItem>();
+
+		public override void SetDefaults()
+		{
+			Item.Size = new Microsoft.Xna.Framework.Vector2(48);
+			Item.value = Item.value = Terraria.Item.buyPrice(0, 0, 40, 0);
+			Item.rare = ItemRarityID.White;
+			Item.maxStack = 99;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.useTime = 10;
+			Item.useAnimation = 15;
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.consumable = true;
+			Item.createTile = ModContent.TileType<TheGoodPainting>();
+		}
+	}
+}
