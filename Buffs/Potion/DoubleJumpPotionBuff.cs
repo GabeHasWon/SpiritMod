3127727ec9230f9e9bcbc00857a7fc49058c5ed8
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2023-10-12 01:33:30 -0300

    Reimplemented additional jumps

--- Buffs/Potion/DoubleJumpPotionBuff.cs
+++ Buffs/Potion/DoubleJumpPotionBuff.cs
@@ -1,4 +1,6 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace SpiritMod.Buffs.Potion
@@ -11,10 +13,42 @@ namespace SpiritMod.Buffs.Potion
 			Main.buffNoTimeDisplay[Type] = false;
 		}
 
-		public override void Update(Player player, ref int buffIndex)
+		public override void Update(Player player, ref int buffIndex) => player.GetJumpState<AetherJump>().Enable();
+	}
+
+	public class AetherJump : ExtraJump
+	{
+		public override Position GetDefaultPosition() => new Before(CloudInABottle);
+
+		public override float GetDurationMultiplier(Player player) => .75f;
+
+		public override void OnStarted(Player player, ref bool playSound)
 		{
-			//player.hasJumpOption_Cloud = true;
+			static void SpawnGore(Player player, Vector2 position)
+			{
+				Gore gore = Gore.NewGoreDirect(player.GetSource_FromThis(), position, -player.velocity, Main.rand.Next(11, 14));
+				gore.velocity.X = gore.velocity.X * 0.1f - player.velocity.X * 0.1f;
+				gore.velocity.Y = gore.velocity.Y * 0.1f - player.velocity.Y * 0.05f;
+			}
+
+			int offsetY = ((player.gravDir == -1f) ? 0 : player.height) - 16;
+			for (int i = 0; i < 10; i++)
+			{
+				Dust dust = Dust.NewDustDirect(player.position + new Vector2(-34f, offsetY), 102, 32, DustID.Cloud, -player.velocity.X * .5f, player.velocity.Y * .5f, 100, default, 1.5f);
+				dust.velocity = dust.velocity * 0.5f - player.velocity * new Vector2(0.1f, 0.3f);
+			}
+
+			SpawnGore(player, player.Top + new Vector2(-16f, offsetY));
+			SpawnGore(player, player.position + new Vector2(-36f, offsetY));
+			SpawnGore(player, player.TopRight + new Vector2(4f, offsetY));
+		}
+
+		public override void ShowVisuals(Player player)
+		{
+			int offsetY = (player.gravDir == -1f) ? -6 : player.height;
+
+			Dust dust = Dust.NewDustDirect(player.position + new Vector2(-4, offsetY), player.width + 8, 4, DustID.Cloud, -player.velocity.X * .5f, player.velocity.Y * .5f, 100, default, 1.5f);
+			dust.velocity = dust.velocity * 0.5f - player.velocity * new Vector2(0.1f, 0.3f);
 		}
 	}
 }
-
--- Items/BossLoot/AvianDrops/ApostleArmor/TalonHeaddress.cs
+++ Items/BossLoot/AvianDrops/ApostleArmor/TalonHeaddress.cs
@@ -1,5 +1,7 @@
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace SpiritMod.Items.BossLoot.AvianDrops.ApostleArmor
@@ -7,12 +9,6 @@ namespace SpiritMod.Items.BossLoot.AvianDrops.ApostleArmor
 	[AutoloadEquip(EquipType.Head)]
 	public class TalonHeaddress : ModItem
 	{
-		public override void SetStaticDefaults()
-		{
-			// DisplayName.SetDefault("Apostle's Headdress");
-			// Tooltip.SetDefault("7% increased magic and ranged critical strike chance");
-		}
-
 		public override void SetDefaults()
 		{
 			Item.width = 38;
@@ -27,7 +23,7 @@ namespace SpiritMod.Items.BossLoot.AvianDrops.ApostleArmor
 		public override void UpdateArmorSet(Player player)
 		{
 			player.setBonus = Language.GetTextValue("Mods.SpiritMod.SetBonuses.Apostle");
-			//player.hasJumpOption_Cloud = true;
+			player.GetJumpState<ApostleJump>().Enable();
 			player.GetSpiritPlayer().talonSet = true;
 		}
 
@@ -35,6 +31,48 @@ namespace SpiritMod.Items.BossLoot.AvianDrops.ApostleArmor
 		{
 			player.GetCritChance(DamageClass.Magic) += 7;
 			player.GetCritChance(DamageClass.Ranged) += 7;
+		}
+	}
+
+	public class ApostleJump : ExtraJump
+	{
+		public override Position GetDefaultPosition() => new Before(CloudInABottle);
+
+		public override float GetDurationMultiplier(Player player) => 1.2f;
+
+		public override void UpdateHorizontalSpeeds(Player player)
+		{
+			player.runAcceleration *= 1.5f;
+			player.maxRunSpeed *= 1.5f;
+		}
+
+		public override void OnStarted(Player player, ref bool playSound)
+		{
+			static void SpawnGore(Player player, Vector2 position)
+			{
+				Gore gore = Gore.NewGoreDirect(player.GetSource_FromThis(), position, -player.velocity, Main.rand.Next(11, 14));
+				gore.velocity.X = gore.velocity.X * 0.1f - player.velocity.X * 0.1f;
+				gore.velocity.Y = gore.velocity.Y * 0.1f - player.velocity.Y * 0.05f;
+			}
+
+			int offsetY = ((player.gravDir == -1f) ? 0 : player.height) - 16;
+			for (int i = 0; i < 10; i++)
+			{
+				Dust dust = Dust.NewDustDirect(player.position + new Vector2(-34f, offsetY), 102, 32, DustID.Cloud, -player.velocity.X * .5f, player.velocity.Y * .5f, 100, default, 1.5f);
+				dust.velocity = dust.velocity * 0.5f - player.velocity * new Vector2(0.1f, 0.3f);
+			}
+
+			SpawnGore(player, player.Top + new Vector2(-16f, offsetY));
+			SpawnGore(player, player.position + new Vector2(-36f, offsetY));
+			SpawnGore(player, player.TopRight + new Vector2(4f, offsetY));
+		}
+
+		public override void ShowVisuals(Player player)
+		{
+			int offsetY = (player.gravDir == -1f) ? -6 : player.height;
+
+			Dust dust = Dust.NewDustDirect(player.position + new Vector2(-4, offsetY), player.width + 8, 4, DustID.Cloud, -player.velocity.X * .5f, player.velocity.Y * .5f, 100, default, 1.5f);
+			dust.velocity = dust.velocity * 0.5f - player.velocity * new Vector2(0.1f, 0.3f);
 		}
 	}
 }
