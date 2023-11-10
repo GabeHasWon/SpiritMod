e1990573c87f36c0a6f9548478123836af2d38d8
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2023-11-10 17:05:56 -0400

    Several tile resprites

--- Tiles/Furniture/Bamboo/BambooBarrel.cs
+++ Tiles/Furniture/Bamboo/BambooBarrel.cs
@@ -1,7 +1,6 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using SpiritMod.Items.Placeable.Furniture.Bamboo;
-using System.Collections.Generic;
 using Terraria;
 using Terraria.Audio;
 using Terraria.DataStructures;
