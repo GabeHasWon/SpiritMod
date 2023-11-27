7f4858bdb225a28c417652a76c5d0277d168fdab
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2023-11-27 14:14:14 -0400

    Adorned Bow and tile fixes

--- Tiles/Furniture/Fountains/BriarFountain.cs
+++ Tiles/Furniture/Fountains/BriarFountain.cs
@@ -1,18 +1,14 @@
-using SpiritMod.Effects.Waters.Reach;
 using Terraria.ModLoader;
 
-namespace SpiritMod.Tiles.Furniture.Fountains
+namespace SpiritMod.Tiles.Furniture.Fountains;
+
+public class BriarFountain : BaseFountain
 {
-	public class BriarFountain : BaseFountain
-    {
-		internal override int DropType => ModContent.ItemType<BriarFountainItem>();
-		internal override int WaterStyle => ModContent.GetInstance<ReachWaterStyle>().Slot;
-	}
+	internal override int DropType => ModContent.ItemType<BriarFountainItem>();
+	//internal override int WaterStyle => ModContent.GetInstance<ReachWaterStyle>().Slot;
+}
 
-	public class BriarFountainItem : BaseFountainItem
-	{
-		internal override int PlaceType => ModContent.TileType<BriarFountain>();
-
-		// public override void SetStaticDefaults() => DisplayName.SetDefault("Briar Water Fountain");
-	}
+public class BriarFountainItem : BaseFountainItem
+{
+	internal override int PlaceType => ModContent.TileType<BriarFountain>();
 }
