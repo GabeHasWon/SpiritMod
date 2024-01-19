9efb8f9f83d183f61a52e7f64dd98953d51943e3
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2024-01-19 19:05:38 -0400

    Skeletron Prime music fix

---
+++ Items/Placeable/MusicBox/SkeletronPrimeBox.cs
@@ -0,0 +1,27 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using SkeletronPrimeBoxTile = SpiritMod.Tiles.MusicBox.SkeletronPrimeBox;
+
+namespace SpiritMod.Items.Placeable.MusicBox
+{
+	[Sacrifice(1)]
+	public class SkeletronPrimeBox : ModItem
+	{
+		public override void SetDefaults()
+		{
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.useTurn = true;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.autoReuse = true;
+			Item.consumable = true;
+			Item.createTile = ModContent.TileType<SkeletronPrimeBoxTile>();
+			Item.width = 24;
+			Item.height = 24;
+			Item.rare = ItemRarityID.LightRed;
+			Item.value = 100000;
+			Item.accessory = true;
+			Item.hasVanityEffects = true;
+		}
+	}
+}
---
+++ Tiles/MusicBox/SkeletronPrimeBox.cs
@@ -0,0 +1,38 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace SpiritMod.Tiles.MusicBox
+{
+	internal class SkeletronPrimeBox : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileObsidianKill[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+			TileObjectData.newTile.Origin = new Point16(0, 1);
+			TileObjectData.newTile.LavaDeath = false;
+			TileObjectData.newTile.DrawYOffset = 2;
+			TileObjectData.addTile(Type);
+			TileID.Sets.DisableSmartCursor[Type] = true;
+
+			LocalizedText name = CreateMapEntryName();
+			AddMapEntry(new Color(200, 200, 200), name);
+			RegisterItemDrop(ModContent.ItemType<Items.Placeable.MusicBox.SkeletronPrimeBox>());
+			DustType = -1;
+        }
+
+		public override void MouseOver(int i, int j)
+		{
+			Player player = Main.LocalPlayer;
+			player.noThrow = 2;
+			player.cursorItemIconEnabled = true;
+			player.cursorItemIconID = ModContent.ItemType<Items.Placeable.MusicBox.SkeletronPrimeBox>();
+		}
+	}
+}
