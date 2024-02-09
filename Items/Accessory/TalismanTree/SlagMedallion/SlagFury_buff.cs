8dd435b3e2f1a3e37c56ad40d87f7913151e3987
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2024-02-09 08:46:47 -0400

    Slag Fury damage fix

--- Items/Accessory/TalismanTree/SlagMedallion/SlagFury_buff.cs
+++ Items/Accessory/TalismanTree/SlagMedallion/SlagFury_buff.cs
@@ -2,6 +2,7 @@
 using Terraria.ModLoader;
 using SpiritMod.GlobalClasses.Players;
 using Terraria.Localization;
+using System;
 
 namespace SpiritMod.Items.Accessory.TalismanTree.SlagMedallion
 {
@@ -13,11 +14,8 @@ namespace SpiritMod.Items.Accessory.TalismanTree.SlagMedallion
 			Main.buffNoTimeDisplay[Type] = false;
 		}
 		public override void ModifyBuffText(ref string buffName, ref string tip, ref int rare) => tip = Language.GetText("Mods.SpiritMod.Buffs.SlagFury_buff.Description")
-			.WithFormatArgs(Main.LocalPlayer.GetModPlayer<TalismanPlayer>().slagDamageMultiplier).Value;
+			.WithFormatArgs(Math.Round(Main.LocalPlayer.GetModPlayer<TalismanPlayer>().slagDamageMultiplier * 100f)).Value;
 
-		public override void Update(Player player, ref int buffIndex)
-		{
-			player.GetDamage(DamageClass.Generic) += player.GetModPlayer<TalismanPlayer>().slagDamageMultiplier;
-		}
+		public override void Update(Player player, ref int buffIndex) => player.GetDamage(DamageClass.Generic) += player.GetModPlayer<TalismanPlayer>().slagDamageMultiplier;
 	}
 }
